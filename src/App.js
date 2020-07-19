05e9a60aac3b92f23f7f6d82ee909797bcec2c30
unknown <saaluissaka>
2020-07-19 08:55:13 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import Nav from './components/layouts/Navbar';
+class componentName extends Component {
+	render() {
+		return (
+			<div className="App">
+				<Nav />
+			</div>
+		);
+	}
+}
+
+export default componentName;
---
+++ src/components/layouts/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class componentName extends Component {
+	render() {
+		return (
+			<nav>
+				<h1>
+					<i className="fab fa-github" />GitHub
+				</h1>
+			</nav>
+		);
+	}
+}
+
+export default componentName;
